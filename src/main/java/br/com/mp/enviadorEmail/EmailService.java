6aa907bf1f4c0dd85637c7f971ce223dfc46a85f
Marcus Paulo <mpaulobr@gmail.com>
2017-02-03 01:27:51 -0200

    Componente de Envio de E-mail do Projeto em SpringBoot ListaVip

---
+++ src/main/java/br/com/mp/enviadorEmail/EmailService.java
@@ -0,0 +1,29 @@
+package br.com.mp.enviadorEmail;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    public void enviar(String nome, String emailConvidado){
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator("email", "senha"));
+            email.setSSLOnConnect(true);
+
+            email.setFrom("contato@listavip.com.br");
+            email.setSubject("Você foi convidado pelo ListaVIP");
+            email.setMsg("Olá " + nome + ". Você acaba de ser convidado pelo ListaVIP.");
+            email.addTo(emailConvidado);
+            email.send();
+
+        } catch (EmailException e) {
+            e.printStackTrace();
+        }
+    }
+}
